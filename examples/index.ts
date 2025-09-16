8c9cc2f44f6dda3e73be791e1d7e71d7745bfd30
anubra266 <anubra266@gmail.com>
2025-09-15 18:32:38 -0700

    format

--- examples/index.ts
+++ examples/index.ts
@@ -1,4 +1,4 @@
-import {   project } from '@inkeep/agents-sdk';
+import { project } from '@inkeep/agents-sdk';
 import { basicGraph } from 'graphs/basic.graph';
 import { weatherGraph } from 'graphs/weather-graph.graph';
 
@@ -6,5 +6,5 @@ export const myProject = project({
   id: 'my-project',
   name: 'My Project',
   description: 'My project',
-  graphs: () => [basicGraph, weatherGraph, ],
+  graphs: () => [basicGraph, weatherGraph],
 });
