3cf49e397e12ebd39412fa298a63352695c995d0
Dima <dima@inkeep.com>
2025-10-08 22:53:58 +0300

    bump monaco-editor

---
+++ agents-manage-ui/__mocks__/monaco-editor.ts
@@ -0,0 +1,77 @@
+// import { defineWebWorkers } from '@vitest/web-worker/pure';
+
+// defineWebWorkers();
+
+/**
+ * Fixes TypeError: Cannot read properties of null (reading 'webkitBackingStorePixelRatio')
+ */
+import 'vitest-canvas-mock';
+
+/**
+ * Fixes TypeError: mainWindow.matchMedia is not a function
+ * @see https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+ */
+if (!window.matchMedia) {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: false,
+    value: vi.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(), // deprecated
+      removeListener: vi.fn(), // deprecated
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    })),
+  });
+}
+
+// /**
+//  * Fixes TypeError: document.queryCommandSupported is not a function
+//  */
+// // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+// if (!navigator.clipboard) {
+//   Object.defineProperty(navigator, 'clipboard', {
+//     writable: false,
+//     value: {
+//       write: async () => null,
+//     },
+//   });
+// }
+//
+// /**
+//  * Fixes ReferenceError: ResizeObserver is not defined
+//  */
+// // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+// if (!window.ResizeObserver) {
+//   Object.defineProperty(window, 'ResizeObserver', {
+//     writable: false,
+//     value: vi.fn().mockReturnValue({
+//       observe() {},
+//       disconnect() {},
+//     }),
+//   });
+// }
+//
+// /**
+//  * ReferenceError: ClipboardItem is not defined
+//  */
+// // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+// if (!window.ClipboardItem) {
+//   Object.defineProperty(window, 'ClipboardItem', {
+//     writable: false,
+//     value: vi.fn(),
+//   });
+// }
+
+// process.on('unhandledRejection', (reason) => {
+//   if (reason instanceof Error && reason.name === 'Canceled' && reason.name === reason.message) {
+//     // ignore DeferredPromise.cancel
+//     return;
+//   }
+//   throw reason;
+// });
+
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports, import-x/no-extraneous-dependencies
+export * from 'monaco-editor';
