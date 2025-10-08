03a116975351e0a5b05c978fd7fe9e1d05cc6bc4
Dima <dima@inkeep.com>
2025-10-08 23:35:55 +0300

    polish

--- agents-manage-ui/__mocks__/monaco-editor.ts
+++ agents-manage-ui/__mocks__/monaco-editor.ts
@@ -1,7 +1,4 @@
-// import { defineWebWorkers } from '@vitest/web-worker/pure';
-
-// defineWebWorkers();
-
+import { defineWebWorkers } from '@vitest/web-worker/pure';
 /**
  * Fixes TypeError: Cannot read properties of null (reading 'webkitBackingStorePixelRatio')
  */
@@ -27,51 +24,6 @@ if (!window.matchMedia) {
   });
 }
 
-// /**
-//  * Fixes TypeError: document.queryCommandSupported is not a function
-//  */
-// // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-// if (!navigator.clipboard) {
-//   Object.defineProperty(navigator, 'clipboard', {
-//     writable: false,
-//     value: {
-//       write: async () => null,
-//     },
-//   });
-// }
-//
-// /**
-//  * Fixes ReferenceError: ResizeObserver is not defined
-//  */
-// // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-// if (!window.ResizeObserver) {
-//   Object.defineProperty(window, 'ResizeObserver', {
-//     writable: false,
-//     value: vi.fn().mockReturnValue({
-//       observe() {},
-//       disconnect() {},
-//     }),
-//   });
-// }
-//
-// /**
-//  * ReferenceError: ClipboardItem is not defined
-//  */
-// // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-// if (!window.ClipboardItem) {
-//   Object.defineProperty(window, 'ClipboardItem', {
-//     writable: false,
-//     value: vi.fn(),
-//   });
-// }
+defineWebWorkers();
 
-// process.on('unhandledRejection', (reason) => {
-//   if (reason instanceof Error && reason.name === 'Canceled' && reason.name === reason.message) {
-//     // ignore DeferredPromise.cancel
-//     return;
-//   }
-//   throw reason;
-// });
-
-// eslint-disable-next-line @typescript-eslint/no-restricted-imports, import-x/no-extraneous-dependencies
 export * from 'monaco-editor';
