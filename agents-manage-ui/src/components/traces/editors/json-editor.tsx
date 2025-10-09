ffc975ac4069441f65259809f3efc64fec0a2064
Dima <dima@inkeep.com>
2025-10-10 01:59:12 +0300

    copy classes

--- agents-manage-ui/src/components/traces/editors/json-editor.tsx
+++ agents-manage-ui/src/components/traces/editors/json-editor.tsx
@@ -129,9 +129,35 @@ export const JsonEditor = forwardRef<
     );
   }, []);
 
+  // h-full
+  // [&>.cm-editor]:max-h-[inherit]
+  // [&>.cm-editor]:!bg-transparent
+  // dark:[&>.cm-editor]:!bg-input/30
+  // [&>.cm-editor]:!outline-none
+  // [&>.cm-editor]:rounded-[7px]
+  // [&>.cm-editor]:px-3
+  // [&>.cm-editor]:py-2
+  // leading-2
+  // text-xs
+  // font-mono
+  // rounded-md
+  // border
+  // border-input
+  // shadow-xs
+  // transition-[color,box-shadow]
+  // data-disabled:cursor-not-allowed
+  // data-disabled:opacity-50
+  // data-disabled:bg-muted
+  // data-invalid:border-destructive
+  // has-[.cm-focused]:border-ring
+  // has-[.cm-focused]:ring-ring/50
+  // has-[.cm-focused]:ring-[3px]
+  // aria-invalid:ring-destructive/20
+  // dark:aria-invalid:ring-destructive/40
+  // aria-invalid:border-destructive',
+
   return (
     <div
-      //
       ref={containerRef}
       className={cn(
         'rounded-xl overflow-hidden border border-input relative shadow-xs',
