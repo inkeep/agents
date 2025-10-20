c0ff252e7cb99f1db3da08404bc17f3b7326be6e
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-10-20 21:50:09 +0300

    Update code-editor.tsx

--- agents-manage-ui/src/components/editors/code-editor.tsx
+++ agents-manage-ui/src/components/editors/code-editor.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { type ComponentProps, type FC, useId, useMemo } from 'react';
+import { type ComponentProps, type FC, useId } from 'react';
 import { MonacoEditor } from './monaco-editor';
 
 interface CodeEditorProps extends Omit<ComponentProps<typeof MonacoEditor>, 'uri'> {
