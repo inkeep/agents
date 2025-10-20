83b2e026a934e235a7013d90cfb8162021ff0972
Dima <dima@inkeep.com>
2025-10-20 03:06:36 +0200

    rm unused

--- agents-manage-ui/src/env.d.ts
+++ agents-manage-ui/src/env.d.ts
@@ -2,7 +2,3 @@ declare namespace globalThis {
   import type * as monaco from 'monaco-editor';
   var MonacoEnvironment: monaco.Environment;
 }
-
-declare module 'monaco-editor/esm/vs/editor/common/core/range.js' {
-  export { Range } from 'monaco-graphql/esm/monaco-editor';
-}
