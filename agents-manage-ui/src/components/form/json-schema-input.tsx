730a35caa127bc2c061c313e287bc76b89d37707
Dima <dima@inkeep.com>
2025-10-27 13:17:48 +0100

    add table

--- agents-manage-ui/src/components/form/json-schema-input.tsx
+++ agents-manage-ui/src/components/form/json-schema-input.tsx
@@ -4,6 +4,7 @@ import type { Control, FieldPath, FieldValues } from 'react-hook-form';
 import { FormFieldWrapper } from './form-field-wrapper';
 import { StandaloneJsonEditor } from '../editors/standalone-json-editor';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Table, TableBody, TableCell, TableHead, TableHeader } from '@/components/ui/table';
 
 interface JsonSchemaInputProps<T extends FieldValues> {
   control: Control<T>;
@@ -44,7 +45,20 @@ export function JsonSchemaInput<T extends FieldValues>({
               Advanced
             </TabsTrigger>
           </TabsList>
-          <TabsContent value="simple">simple</TabsContent>
+          <TabsContent value="simple">
+            <Table>
+              <TableHeader>
+                <TableHead>Name</TableHead>
+                <TableHead>Type</TableHead>
+                <TableHead>Value</TableHead>
+              </TableHeader>
+              <TableBody>
+                <TableCell>Foo</TableCell>
+                <TableCell>Bar</TableCell>
+                <TableCell>Qyz</TableCell>
+              </TableBody>
+            </Table>
+          </TabsContent>
           <TabsContent value="advanced">
             <StandaloneJsonEditor
               placeholder={placeholder}
