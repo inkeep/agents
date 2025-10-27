91a774cffd9d8e995655704038775d258d2c5a2c
Dima <dima@inkeep.com>
2025-10-27 21:21:42 +0100

    add ArrayFieldItemTemplate.tsx

---
+++ agents-manage-ui/src/components/form/ArrayFieldItemTemplate.tsx
@@ -0,0 +1,40 @@
+import {
+  type ArrayFieldItemTemplateProps,
+  type FormContextType,
+  getTemplate,
+  getUiOptions,
+  type RJSFSchema,
+  type StrictRJSFSchema,
+} from '@rjsf/utils';
+
+/** The `ArrayFieldItemTemplate` component is the template used to render an items of an array.
+ *
+ * @param props - The `ArrayFieldItemTemplateProps` props for the component
+ */
+export default function ArrayFieldItemTemplate<
+  T = any,
+  S extends StrictRJSFSchema = RJSFSchema,
+  F extends FormContextType = any,
+>(props: ArrayFieldItemTemplateProps<T, S, F>) {
+  const { children, buttonsProps, hasToolbar, uiSchema, registry } = props;
+  const uiOptions = getUiOptions<T, S, F>(uiSchema);
+  const ArrayFieldItemButtonsTemplate = getTemplate<'ArrayFieldItemButtonsTemplate', T, S, F>(
+    'ArrayFieldItemButtonsTemplate',
+    registry,
+    uiOptions
+  );
+  return (
+    <div>
+      <div className="mb-2 flex flex-row flex-wrap items-center">
+        <div className="grow shrink">{children}</div>
+        <div className="flex items-end justify-end p-0.5">
+          {hasToolbar && (
+            <div className="flex gap-2">
+              <ArrayFieldItemButtonsTemplate {...buttonsProps} />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
