dcbe8e24530846d128bae4496a9dc8f2fc2b5d8a
Dima <dima@inkeep.com>
2025-10-31 14:16:56 +0100

    test passes, misses isRequired

--- agents-manage-ui/src/components/form/__tests__/json-schema-builder.test.ts
+++ agents-manage-ui/src/components/form/__tests__/json-schema-builder.test.ts
@@ -1,5 +1,8 @@
 import type { JSONSchema7 } from 'json-schema';
-import { convertJsonSchemaToFields } from '@/components/form/json-schema-builder';
+import {
+  convertJsonSchemaToFields,
+  fieldsToJsonSchema,
+} from '@/components/form/json-schema-builder';
 import { JSONSchemaFixture } from './json-schema-fixture';
 
 const schemaWithRef: JSONSchema7 = {
@@ -100,76 +103,79 @@ const schemaWithAnyOfResult: JSONSchema7 = {
 describe('convertJsonSchemaToFields', () => {
   it('should converts json schema to fields', () => {
     const schema = convertJsonSchemaToFields(JSONSchemaFixture);
-    expect(schema).toMatchInlineSnapshot(`
-      {
-        "properties": [
-          {
-            "description": "nested object",
-            "isRequired": true,
-            "name": "nested",
-            "properties": [
-              {
-                "description": "another nested object",
-                "name": "nested2",
-                "properties": [
-                  {
-                    "description": "string description",
-                    "isRequired": true,
-                    "name": "string",
-                    "type": "string",
-                  },
-                  {
-                    "description": "number description",
-                    "name": "number",
-                    "type": "number",
-                  },
-                  {
-                    "description": "integer description",
-                    "isRequired": true,
-                    "name": "integer",
-                    "type": "number",
-                  },
-                  {
-                    "description": "boolean description",
-                    "name": "boolean",
-                    "type": "boolean",
-                  },
-                  {
-                    "description": "enum description",
-                    "isRequired": true,
-                    "name": "enum",
-                    "type": "enum",
-                    "values": [
-                      "foo",
-                      "bar",
-                      "baz",
+    expect(schema).toEqual({
+      properties: [
+        {
+          description: 'nested object',
+          isRequired: true,
+          name: 'nested',
+          properties: [
+            {
+              description: 'another nested object',
+              name: 'nested2',
+              properties: [
+                {
+                  description: 'string description',
+                  isRequired: true,
+                  name: 'string',
+                  type: 'string',
+                },
+                {
+                  description: 'number description',
+                  name: 'number',
+                  type: 'number',
+                },
+                {
+                  description: 'integer description',
+                  isRequired: true,
+                  name: 'integer',
+                  type: 'number',
+                },
+                {
+                  description: 'boolean description',
+                  name: 'boolean',
+                  type: 'boolean',
+                },
+                {
+                  description: 'enum description',
+                  isRequired: true,
+                  name: 'enum',
+                  type: 'enum',
+                  values: ['foo', 'bar', 'baz'],
+                },
+                {
+                  description: 'array description',
+                  items: {
+                    description: 'array item description',
+                    properties: [
+                      {
+                        description: 'array string item description',
+                        name: 'prop',
+                        type: 'string',
+                      },
                     ],
+                    type: 'object',
                   },
-                  {
-                    "description": "array description",
-                    "items": {
-                      "description": "array item description",
-                      "properties": [
-                        {
-                          "description": "array string item description",
-                          "name": "prop",
-                          "type": "string",
-                        },
-                      ],
-                      "type": "object",
-                    },
-                    "name": "array",
-                    "type": "array",
-                  },
-                ],
-                "type": "object",
-              },
-            ],
-            "type": "object",
-          },
-        ],
-        "type": "object",
-      }
-    `);
+                  name: 'array',
+                  type: 'array',
+                },
+              ],
+              type: 'object',
+            },
+          ],
+          type: 'object',
+        },
+      ],
+      type: 'object',
+    });
+  });
+
+  it('should fallback when JSON schema contains $ref', () => {
+    const schema = convertJsonSchemaToFields(schemaWithRef);
+    expect(fieldsToJsonSchema(schema)).toEqual(schemaWithRefResult);
+  });
+  it('should fallback when JSON schema contains anyOf', () => {
+    const schema = convertJsonSchemaToFields(schemaWithAnyOf);
+    expect(fieldsToJsonSchema(schema)).toEqual(schemaWithAnyOfResult);
   });
 });
