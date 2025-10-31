8b889f4c610350bc483ded9db5049d1b97df2df8
tim-inkeep <132074086+tim-inkeep@users.noreply.github.com>
2025-10-31 15:10:01 -0400

    updated model docs (#763)
    * updated model docs
    
    * forgot to add the new models page
    
    * updated docs again
    
    * one line changed
    
    * updataed docs
    
    * updated model docs
    
    * updated that too
    
    ---------
    
    Co-authored-by: robert-inkeep <123133375+robert-inkeep@users.noreply.github.com>

--- agents-docs/navigation.ts
+++ agents-docs/navigation.ts
@@ -36,6 +36,7 @@ export default {
       icon: 'LuCode',
       pages: [
         'typescript-sdk/agent-settings',
+        'typescript-sdk/models',
         'typescript-sdk/agent-relationships',
         {
           group: 'Tools',
