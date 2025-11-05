3a0415f8f3b9f0a7402ac66ecf6282972795aefd
Dima <dima@inkeep.com>
2025-11-05 18:33:43 +0100

    add `ignoreIssues`

--- agents-manage-ui/knip.config.ts
+++ agents-manage-ui/knip.config.ts
@@ -3,4 +3,8 @@ import type { KnipConfig } from 'knip';
 export default {
   tags: ['-lintignore'],
   ignoreDependencies: ['@svgr/webpack'],
+  ignoreIssues: {
+    'src/components/ui/*': ['exports'],
+    'src/components/agent/configuration/model-options.tsx': ['exports'],
+  },
 } satisfies KnipConfig;
