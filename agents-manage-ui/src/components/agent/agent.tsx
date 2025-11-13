6fe0005a8237146ddfda75172bfe1cf849c5c8be
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-11-13 01:47:15 +0100

    dashboard: fix `WARNING: Panel defaultSize prop recommended to avoid layout shift after server rendering` (#908)
    * Add defaultSize prop to panel to prevent layout shift
    
    Added defaultSize prop to avoid layout shift.
    
    * Fix warning for Panel defaultSize prop
    
    * Update agents-manage-ui/src/components/agent/agent.tsx

--- agents-manage-ui/src/components/agent/agent.tsx
+++ agents-manage-ui/src/components/agent/agent.tsx
@@ -865,6 +865,8 @@ function AgentReactFlowConsumer({
         id="react-flow-pane"
         order={1}
         minSize={30}
+        // fixes WARNING: Panel defaultSize prop recommended to avoid layout shift after server rendering
+        defaultSize={100}
         className="relative"
       >
         <DefaultMarker />
