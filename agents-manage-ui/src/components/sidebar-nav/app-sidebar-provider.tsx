a2843381439a5d3f60ed3fcd4884260a07fb5d4d
Dima <dima@inkeep.com>
2025-11-18 14:42:33 +0800

    more

--- agents-manage-ui/src/components/sidebar-nav/app-sidebar-provider.tsx
+++ agents-manage-ui/src/components/sidebar-nav/app-sidebar-provider.tsx
@@ -6,8 +6,8 @@ import { SidebarProvider } from '@/components/ui/sidebar';
 
 export const AppSidebarProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [isSidebarOpenInternal, setSidebarOpenInternal] = useState(false);
-  const isOpen = isSidebarOpen || isSidebarOpenInternal;
+  const [isSidebarHoverOpen, setIsSidebarHoverOpen] = useState(false);
+  const isOpen = isSidebarOpen || isSidebarHoverOpen;
 
   return (
     <SidebarProvider
@@ -18,7 +18,7 @@ export const AppSidebarProvider: FC<{ children: ReactNode }> = ({ children }) =>
       open={isOpen}
       onOpenChange={setIsSidebarOpen}
     >
-      <AppSidebar open={isOpen} setOpen={setSidebarOpenInternal} />
+      <AppSidebar open={isOpen} setOpen={setIsSidebarHoverOpen} />
       {children}
     </SidebarProvider>
   );
