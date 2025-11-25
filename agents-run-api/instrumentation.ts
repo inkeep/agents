a56566fe1d6c942fec03e592d818eb33981d5806
shagun-singh-inkeep <shagun.singh@inkeep.com>
2025-11-24 22:55:49 -0600

    fix

--- agents-run-api/instrumentation.ts
+++ agents-run-api/instrumentation.ts
@@ -1,11 +1,9 @@
 import { registerOTel } from '@vercel/otel';
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await registerOTel({
-      serviceName: 'inkeep-agents-run-api',
-      traceExporter: 'otlp-http',
-    });
-  }
+  await registerOTel({
+    serviceName: 'inkeep-agents-run-api',
+    traceExporter: 'otlp-http',
+  });
 }
 
