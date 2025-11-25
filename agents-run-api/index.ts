1215505f7628402e111546c3b8ce58f900b38ae1
shagun-singh-inkeep <shagun.singh@inkeep.com>
2025-11-25 15:37:07 -0600

    delete

--- agents-run-api/index.ts
+++
@@ -1,9 +0,0 @@
-import './src/env';
-import { defaultSDK } from './src/instrumentation';
-
-defaultSDK.start();
-
-import app from './src/index';
-
-export const runtime = 'nodejs';
-export default app;
