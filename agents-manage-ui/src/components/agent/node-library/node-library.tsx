0dd8b097ad7e66bab1413eb42838c3c7357a5fc3
Dima <dima@inkeep.com>
2025-11-27 00:01:44 +0800

    upd

--- agents-manage-ui/src/components/agent/node-library/node-library.tsx
+++ agents-manage-ui/src/components/agent/node-library/node-library.tsx
@@ -12,22 +12,18 @@ const nodeTypes: NodeItem[] = [
 
 export default function NodeLibrary() {
   return (
-    <div className="flex flex-col gap-2 max-w-72 w-40 min-w-0">
-      <div className="bg-red-500 group/node-stack flex flex-col gap-0 group-hover/node-stack:gap-2 group-focus-within/node-stack:gap-2">
+    <div className="flex flex-col gap-2 w-40">
+      <div className="group/node-stack flex flex-col gap-0 group-hover/node-stack:gap-2 group-focus-within/node-stack:gap-2">
         <NodeItem node={nodeTypeMap[NodeType.SubAgent]} />
         <div
           className={cn(
             'opacity-0 max-h-0',
             'flex flex-col gap-2',
             'transition-[max-height,opacity,transform] duration-300',
-            'group-hover/node-stack:max-h-[400px]',
-            'group-hover/node-stack:opacity-100',
-            'group-hover/node-stack:translate-y-0',
-            'group-hover/node-stack:pointer-events-auto',
-            'group-focus-within/node-stack:max-h-[400px]',
-            'group-focus-within/node-stack:opacity-100',
-            'group-focus-within/node-stack:translate-y-0',
-            'group-focus-within/node-stack:pointer-events-auto'
+            'pointer-events-none',
+            'group-hover/node-stack:pointer-events-auto group-focus-within/node-stack:pointer-events-auto',
+            'group-hover/node-stack:max-h-100 group-focus-within/node-stack:max-h-100',
+            'group-hover/node-stack:opacity-100 group-focus-within/node-stack:opacity-100'
           )}
         >
           {nodeTypes.map((node) => (
