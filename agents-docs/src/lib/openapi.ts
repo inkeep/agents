87d1c0ae8c5373956c7768fde019e82b05eac5ca
Dima <dima@inkeep.com>
2025-12-08 15:09:48 +0700

    use legacy openapi json temporarily until docs openapi docs will be split into multiple pages

--- agents-docs/src/lib/openapi.ts
+++ agents-docs/src/lib/openapi.ts
@@ -3,7 +3,7 @@ import { createOpenAPI } from 'fumadocs-openapi/server';
 export const openapi = createOpenAPI({
   // the OpenAPI schema, you can also give it an external URL.
   input: () => ({
-    index: 'https://agents-manage-api.preview.inkeep.com/openapi.json',
+    index: 'https://github.com/inkeep/agents/blob/b1662340abdb842477389c13f22382a54c1ede75/agents-docs/src/lib/index.json',
     'run-api': 'https://agents-run-api.preview.inkeep.com/openapi.json',
   }),
 });
