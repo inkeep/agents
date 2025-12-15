e6434f5343502c9aca799ef79d1909b051a59049
Dima <dima@inkeep.com>
2025-12-15 22:12:00 +0700

    add deps to useEditor

--- agents-manage-ui/src/components/editors/prompt-editor.tsx
+++ agents-manage-ui/src/components/editors/prompt-editor.tsx
@@ -8,7 +8,7 @@ import { EditorContent, type UseEditorOptions, useEditor } from '@tiptap/react';
 import { StarterKit } from '@tiptap/starter-kit';
 import { TextInitial } from 'lucide-react';
 import type { ComponentPropsWithoutRef, FC, RefObject } from 'react';
-import { useCallback, useEffect, useImperativeHandle, useState } from 'react';
+import { useEffect, useImperativeHandle, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { useAgentActions, useAgentStore } from '@/features/agent/state/use-agent-store';
 import { MarkdownIcon } from '@/icons';
@@ -96,33 +96,35 @@ export const PromptEditor: FC<PromptEditorProps> = ({
   const isMarkdownMode = useAgentStore((state) => state.isMarkdownEditor);
   const editable = !(readOnly || disabled);
 
-  const editor = useEditor({
-    ...editorOptions,
-    autofocus: autoFocus,
-    // to see placeholder on initial rendering
-    immediatelyRender: true,
-    editorProps: {
-      attributes: {
-        class: cn(
-          'prose prose-sm dark:prose-invert',
-          'focus:outline-none overflow-scroll min-w-full',
-          'dark:bg-input/30 text-sm focus:outline-none px-3 py-2',
-          'rounded-md border border-input shadow-xs transition-colors',
-          hasDynamicHeight ? 'min-h-16' : 'min-h-80',
-          editable
-            ? 'focus-within:border-ring focus-within:ring-2 focus-within:ring-ring/40'
-            : 'bg-muted/50 text-muted-foreground opacity-70 cursor-not-allowed',
-          ariaInvalid === 'true' &&
-            'border-destructive focus-within:border-destructive focus-within:ring-destructive/30',
-          className
-        ),
+  const editor = useEditor(
+    {
+      ...editorOptions,
+      editable,
+      autofocus: autoFocus,
+      // to see placeholder on initial rendering
+      immediatelyRender: true,
+      editorProps: {
+        attributes: {
+          class: cn(
+            'prose prose-sm dark:prose-invert',
+            'focus:outline-none overflow-scroll min-w-full',
+            'dark:bg-input/30 text-sm focus:outline-none px-3 py-2',
+            'rounded-md border border-input shadow-xs transition-colors',
+            hasDynamicHeight ? 'min-h-16' : 'min-h-80',
+            editable
+              ? 'focus-within:border-ring focus-within:ring-2 focus-within:ring-ring/40'
+              : 'bg-muted/50 text-muted-foreground opacity-70 cursor-not-allowed',
+            ariaInvalid === 'true' &&
+              'border-destructive focus-within:border-destructive focus-within:ring-destructive/30',
+            className
+          ),
+        },
       },
-    },
-    extensions: [
-      StarterKit.configure(
+      extensions: [
         isMarkdownMode
-          ? {}
-          : {
+          ? StarterKit
+          : StarterKit.configure({
+              // this is needed to remove node formatting for text mode
               bold: false,
               italic: false,
               orderedList: false,
@@ -132,22 +134,23 @@ export const PromptEditor: FC<PromptEditorProps> = ({
               strike: false,
               heading: false,
               blockquote: false,
-            }
-      ),
-      Placeholder.configure({ placeholder }),
-      Markdown,
-      TaskList,
-      TaskItem.configure({ nested: true }),
-      TableKit,
-      variableSuggestionExtension,
-    ],
-    content: value,
-    contentType: isMarkdownMode ? 'markdown' : undefined,
-    onUpdate({ editor }) {
-      const nextValue = isMarkdownMode ? editor.getMarkdown() : editor.getText();
-      onChange?.(nextValue);
+            }),
+        Placeholder.configure({ placeholder }),
+        Markdown,
+        TaskList,
+        TaskItem.configure({ nested: true }),
+        TableKit,
+        variableSuggestionExtension,
+      ],
+      content: value,
+      contentType: isMarkdownMode ? 'markdown' : undefined,
+      onUpdate({ editor }) {
+        const nextValue = isMarkdownMode ? editor.getMarkdown() : editor.getText();
+        onChange?.(nextValue);
+      },
     },
-  });
+    [isMarkdownMode, editable]
+  );
 
   useEffect(() => {
     if (!editor) {
@@ -157,10 +160,6 @@ export const PromptEditor: FC<PromptEditorProps> = ({
     // and loaded only after focusing editor, this dispatch fix it
     editor.view.dispatch(editor.state.tr.setMeta('placeholder-init', true));
   }, [editor]);
-
-  useEffect(() => {
-    editor?.setEditable(editable);
-  }, [editor, editable]);
 
   useImperativeHandle(
     ref,
@@ -187,17 +186,6 @@ export const PromptEditor: FC<PromptEditorProps> = ({
     [editor]
   );
 
-  const toggle = useCallback(() => {
-    if (!editor) return;
-    // First toggle and after set content, since we are checking current `contentType` in `renderHTML()` of
-    // `variableSuggestionExtension`
-    toggleMarkdownEditor();
-    editor.commands.setContent(
-      isMarkdownMode ? /* text */ editor.getMarkdown() : /* markdown */ editor.getText(),
-      isMarkdownMode ? editorOptions : { contentType: 'markdown' }
-    );
-  }, [editor, isMarkdownMode, toggleMarkdownEditor]);
-
   const IconToUse = isMarkdownMode ? TextInitial : MarkdownIcon;
 
   return (
@@ -207,7 +195,7 @@ export const PromptEditor: FC<PromptEditorProps> = ({
         className="absolute end-2 top-2 z-1"
         size="icon-sm"
         title={`Switch to ${isMarkdownMode ? 'Text' : 'Markdown'}`}
-        onClick={toggle}
+        onClick={toggleMarkdownEditor}
       >
         <IconToUse />
       </Button>
