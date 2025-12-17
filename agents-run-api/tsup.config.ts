de333c053eca8b395305b3334554677c765a2029
shagun-singh-inkeep <shagun.singh@inkeep.com>
2025-12-17 17:14:16 -0500

    fix: bundle workflow packages in agents-run-api to fix Vercel deployment

--- agents-run-api/tsup.config.ts
+++ agents-run-api/tsup.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
   ...rootConfig,
   entry: ['src/index.ts', 'src/instrumentation.ts'],
   external: ['keytar'],
+  // Don't externalize workflow packages - they need to be bundled
+  // because they use dynamic imports that Vercel NFT can't trace
+  noExternal: ['workflow', '@workflow/world-postgres', '@workflow/world-vercel', /^@workflow\/.*/],
   esbuildOptions(options) {
     options.loader = {
       ...options.loader,
