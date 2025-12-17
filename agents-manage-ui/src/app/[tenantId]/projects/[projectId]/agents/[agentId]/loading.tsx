a3910d276d029cd79feba633049c5e5882243e82
Dima <dima@inkeep.com>
2025-12-17 19:16:53 +0700

    update xyflow

--- agents-manage-ui/src/app/[tenantId]/projects/[projectId]/agents/[agentId]/loading.tsx
+++ agents-manage-ui/src/app/[tenantId]/projects/[projectId]/agents/[agentId]/loading.tsx
@@ -2,28 +2,35 @@ import type { FC } from 'react';
 import { BodyTemplate } from '@/components/layout/body-template';
 import { Skeleton } from '@/components/ui/skeleton';
 
-const AgentLoadingSkeleton: FC = () => {
+export const AgentLoadingSkeleton: FC = () => {
   return (
-    <BodyTemplate breadcrumbs={[{ label: 'Agents' }, { label: '' }]}>
-      <div className="flex h-screen bg-muted/20 dark:bg-background p-4">
-        <div className="w-52">
-          <div className="space-y-2">
-            <Skeleton className="h-12 w-full rounded-lg" />
-            <Skeleton className="h-12 w-full rounded-lg" />
-          </div>
+    <div className="flex h-screen bg-muted/20 dark:bg-background p-4">
+      <div className="flex flex-col gap-2" style={{ width: 160 }}>
+        <Skeleton style={{ height: 38 }} />
+        <Skeleton style={{ height: 38 }} />
+        <Skeleton style={{ height: 38 }} />
+        <Skeleton style={{ height: 38 }} />
+        <Skeleton style={{ height: 38 }} />
+      </div>
+      <div className="flex-1 flex flex-col">
+        <div className="flex justify-end gap-2 h-9">
+          <Skeleton style={{ width: 84 }} />
+          <Skeleton style={{ width: 100 }} />
+          <Skeleton style={{ width: 127 }} />
+          <Skeleton style={{ width: 168 }} />
         </div>
-        <div className="flex-1 flex flex-col">
-          <div className="flex justify-end gap-3">
-            <Skeleton className="h-9 w-20" />
-            <Skeleton className="h-9 w-16" />
-          </div>
-          <div className="w-64 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Skeleton className="h-24 w-full rounded-lg" />
-          </div>
+        <div className="w-64 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Skeleton className="h-36 w-full rounded-lg" />
         </div>
       </div>
-    </BodyTemplate>
+    </div>
   );
 };
 
-export default AgentLoadingSkeleton;
+export default function Loading() {
+  return (
+    <BodyTemplate breadcrumbs={[{ label: 'Agents' }, { label: '' }]}>
+      <AgentLoadingSkeleton />
+    </BodyTemplate>
+  );
+}
