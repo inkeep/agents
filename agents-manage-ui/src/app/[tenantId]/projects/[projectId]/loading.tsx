a8d8b50e46e47a4743dd6587cb78861fd510ef7c
Dima <dima@inkeep.com>
2025-12-18 22:39:04 +0700

    polish

--- agents-manage-ui/src/app/[tenantId]/projects/[projectId]/loading.tsx
+++ agents-manage-ui/src/app/[tenantId]/projects/[projectId]/loading.tsx
@@ -1,8 +1,9 @@
+import { BodyTemplate } from '@/components/layout/body-template';
 import { Skeleton } from '@/components/ui/skeleton';
 
 export default function Loading() {
   return (
-    <>
+    <BodyTemplate breadcrumbs={[]}>
       <Skeleton className="h-7 mb-2" style={{ width: 70 }} />
       <Skeleton className="h-5 mb-8" style={{ width: 420 }} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 md:gap-4">
@@ -10,6 +11,6 @@ export default function Loading() {
           <Skeleton key={i} className="h-36 w-full rounded-lg" />
         ))}
       </div>
-    </>
+    </BodyTemplate>
   );
 }
