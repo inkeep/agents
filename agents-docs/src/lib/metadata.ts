deb27d54d450361167dcdcce2354cdffeff5f2b5
Dima <dima@inkeep.com>
2025-12-19 18:17:51 +0700

    setup knip for agents-docs

--- agents-docs/src/lib/metadata.ts
+++ agents-docs/src/lib/metadata.ts
@@ -24,7 +24,7 @@ export function createMetadata(override: Metadata): Metadata {
   };
 }
 
-export const baseUrl =
+const baseUrl =
   process.env.NODE_ENV === 'development' || !process.env.VERCEL_URL
     ? new URL('http://localhost:3000')
     : new URL(`https://${process.env.VERCEL_URL}`);
