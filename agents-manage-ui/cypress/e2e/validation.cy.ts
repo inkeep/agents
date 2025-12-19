2a88d2f60afa362ba60b119fb31446da81f03c02
Dima <dima@inkeep.com>
2025-12-19 18:07:56 +0700

    simplify

--- agents-manage-ui/cypress/e2e/validation.cy.ts
+++ agents-manage-ui/cypress/e2e/validation.cy.ts
@@ -3,7 +3,6 @@
 describe('Validation', () => {
   it('should not allow save invalid JSON', () => {
     cy.visit('/default/projects/my-weather-project/agents/weather-agent?pane=agent');
-    cy.get('.monaco-editor').should('be.visible');
     cy.typeInMonaco('contextVariables.json', 'foo bar');
     cy.contains('Save changes').click();
     cy.get('[data-sonner-toast]').should('be.visible');
@@ -12,8 +11,9 @@ describe('Validation', () => {
   });
 
   it('should not allow save empty id', () => {
-    cy.visit('/default/projects/my-weather-project/agents/weather-agent');
-    cy.get('.react-flow__node').eq(1).click();
+    cy.visit(
+      '/default/projects/my-weather-project/agents/weather-agent?pane=node&nodeId=weather-assistant'
+    );
     cy.get('[name=id]').clear();
 
     cy.contains('Save changes').click();
