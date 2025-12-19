d75ab7b5b1000836f301ca937010fbd2566007d2
Dima <dima@inkeep.com>
2025-12-19 18:10:03 +0700

    better assertion

--- agents-manage-ui/cypress/e2e/validation.cy.ts
+++ agents-manage-ui/cypress/e2e/validation.cy.ts
@@ -5,7 +5,7 @@ describe('Validation', () => {
     cy.visit('/default/projects/my-weather-project/agents/weather-agent?pane=agent');
     cy.typeInMonaco('contextVariables.json', 'foo bar');
     cy.contains('Save changes').click();
-    cy.get('[data-sonner-toast]').should('be.visible');
+    cy.get('[data-sonner-toast][data-type=error]').should('be.visible');
     cy.contains('Save changes').should('not.be.disabled');
     cy.contains('Agent saved', { timeout: 0 }).should('not.exist');
   });
@@ -15,9 +15,8 @@ describe('Validation', () => {
       '/default/projects/my-weather-project/agents/weather-agent?pane=node&nodeId=weather-assistant'
     );
     cy.get('[name=id]').clear();
-
     cy.contains('Save changes').click();
-    cy.get('[data-sonner-toast]').should('be.visible');
+    cy.get('[data-sonner-toast][data-type=error]').should('be.visible');
     cy.contains('Save changes').should('not.be.disabled');
     cy.contains('Agent saved', { timeout: 0 }).should('not.exist');
   });
