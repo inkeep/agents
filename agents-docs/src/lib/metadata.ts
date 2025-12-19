96484d32831ea3d0f66cf4e3772c2f498c88ee17
Dima <dima@inkeep.com>
2025-12-19 18:24:42 +0700

    lint fix

--- agents-docs/src/lib/metadata.ts
+++ agents-docs/src/lib/metadata.ts
@@ -23,8 +23,3 @@ export function createMetadata(override: Metadata): Metadata {
     },
   };
 }
-
-const baseUrl =
-  process.env.NODE_ENV === 'development' || !process.env.VERCEL_URL
-    ? new URL('http://localhost:3000')
-    : new URL(`https://${process.env.VERCEL_URL}`);
