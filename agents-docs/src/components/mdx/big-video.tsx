50e6d1979ab250c5743bfeafb72275e2d164b9fd
Gaurav Varma <gaurav@inkeep.com>
2025-12-22 11:55:09 -0800

    added composio vid

---
+++ agents-docs/src/components/mdx/big-video.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface BigVideoProps {
+  src: string;
+  maxWidth?: string;
+  height?: string;
+}
+
+export function BigVideo({ 
+  src, 
+  height = 'auto' 
+}: BigVideoProps) {
+  return (
+    <video
+      src={src}
+      controls
+      style={{
+        borderRadius: '10px',
+        display: 'block',
+        maxWidth: "800px",
+        width: '100%',
+        height: height,
+        margin: '0 auto',
+      }}
+    >
+      Your browser does not support the video tag.
+    </video>
+  );
+}
