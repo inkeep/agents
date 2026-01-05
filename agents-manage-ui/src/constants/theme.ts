53130c7637376a94cdc6a015dff81bc1a26063ea
Dima <dima@inkeep.com>
2026-01-06 00:24:11 +0700

    breadcrumbs

--- agents-manage-ui/src/constants/theme.ts
+++ agents-manage-ui/src/constants/theme.ts
@@ -1,51 +1,30 @@
-// import type { editor } from 'monaco-editor';
-
-export const MONACO_THEME_NAME = {
+export const MONACO_THEME_NAME = Object.freeze({
   light: 'inkeep-light',
   dark: 'inkeep-dark',
-} as const;
-
-// const color = {
-//   transparent: '#00000000',
-// };
-
-// const baseColors: editor.IColors = {
-//   'editor.background': color.transparent,
-//   focusBorder: color.transparent, // Removes blue border
-//   'editor.lineHighlightBorder': color.transparent,
-//   'editor.wordHighlightBackground': color.transparent,
-// };
+});
 
 export const TEMPLATE_LANGUAGE = 'template';
 export const VARIABLE_TOKEN = 'variable';
 
-// export const MONACO_THEME_DATA: Record<'light' | 'dark', editor.IStandaloneThemeData> = {
-//   light: {
-//     base: 'vs',
-//     inherit: true,
-//     rules: [
-//       { token: 'string.key.json', foreground: '#b29762' },
-//       { token: 'string.value.json', foreground: '#1659df' },
-//       { token: `${VARIABLE_TOKEN}.${TEMPLATE_LANGUAGE}`, foreground: '#e67e22', fontStyle: 'bold' },
-//     ],
-//     colors: {
-//       ...baseColors,
-//       'editor.placeholder.foreground': '#58534d',
-//       'editor.lineHighlightBackground': '#ddceb154',
-//     },
-//   },
-//   dark: {
-//     base: 'vs-dark',
-//     inherit: true,
-//     rules: [
-//       { token: 'string.key.json', foreground: '#9a86fd' },
-//       { token: 'string.value.json', foreground: '#ffb870' },
-//       { token: `${VARIABLE_TOKEN}.${TEMPLATE_LANGUAGE}`, foreground: '#f39c12', fontStyle: 'bold' },
-//     ],
-//     colors: {
-//       ...baseColors,
-//       'editor.placeholder.foreground': '#9797a1',
-//       'editor.lineHighlightBackground': '#3633427f',
-//     },
-//   },
-// };
+/**
+ * Used in `/[tenantId]/@breadcrumbs/[...slug]/page.tsx` parallel route and sidebar-nav/app-sidebar
+ * In the future can be used for i18n.
+ */
+export const STATIC_LABELS = Object.freeze({
+  projects: 'Projects',
+  agents: 'Agents',
+  'api-keys': 'API Keys',
+  artifacts: 'Artifacts',
+  settings: 'Settings',
+  traces: 'Traces',
+  credentials: 'Credentials',
+  components: 'Components',
+  'external-agents': 'External Agents',
+  'mcp-servers': 'MCP Servers',
+  bearer: 'Bearer',
+  edit: 'Edit',
+  providers: 'Providers',
+  'tool-calls': 'Tool Calls',
+  'ai-calls': 'AI Calls',
+  conversations: 'Conversations',
+});
