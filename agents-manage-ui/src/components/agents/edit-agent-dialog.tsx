2d0d77a7a50bcd09163e7a823f288286669364cf
sarah <129242944+sarah-inkeep@users.noreply.github.com>
2026-01-16 09:45:48 -0800

    Prd 5600 (#1448)
    * Add ability to edit agent name and description from the agent card
    
    * Add changeset
    
    * Code cleanup
    
    * Fix bug with description not being set on create

---
+++ agents-manage-ui/src/components/agents/edit-agent-dialog.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { AgentForm, type AgentFormData } from './agent-form';
+
+interface EditAgentDialogProps {
+  tenantId: string;
+  projectId: string;
+  agentData: AgentFormData;
+  isOpen: boolean;
+  setIsOpen: (open: boolean) => void;
+}
+
+export function EditAgentDialog({
+  tenantId,
+  projectId,
+  agentData,
+  isOpen,
+  setIsOpen,
+}: EditAgentDialogProps) {
+  const handleSuccess = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogContent className="!max-w-2xl">
+        <DialogHeader>
+          <DialogTitle>Edit agent</DialogTitle>
+          <DialogDescription className="sr-only">Edit agent details.</DialogDescription>
+        </DialogHeader>
+        <AgentForm
+          projectId={projectId}
+          agentId={agentData.id}
+          initialData={agentData}
+          tenantId={tenantId}
+          onSuccess={handleSuccess}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+}
