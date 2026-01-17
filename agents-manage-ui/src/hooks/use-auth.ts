89dbd2c5c6549d71605f7952a209456795ffa693
Dima <dima@inkeep.com>
2026-01-18 05:15:59 +0700

    fix compiler errors

--- agents-manage-ui/src/hooks/use-auth.ts
+++ agents-manage-ui/src/hooks/use-auth.ts
@@ -1,6 +1,11 @@
 import { useAuthClient } from '@/lib/auth-client';
 
 export function useAuthSession() {
+  'use no memo';
+  // TODO:
+  // Can't optimize with react compiler due error
+  // Error: Hooks must be the same function on every render, but this value may change over time to a different function.
+  // See https://react.dev/reference/rules/react-calls-components-and-hooks#dont-dynamically-use-hooks
   const client = useAuthClient();
 
   const { data: session, isPending, error } = client.useSession();
