8556a34b037ed6b2db4f62ad5e10c3db9d9e83f9
Dima <dima@inkeep.com>
2026-01-18 22:06:06 +0700

    upd

--- agents-docs/src/components/ui/collapsible.tsx
+++ agents-docs/src/components/ui/collapsible.tsx
@@ -1,19 +1,19 @@
 import * as CollapsiblePrimitive from '@radix-ui/react-collapsible';
-import { forwardRef } from 'react';
+import type { FC, ComponentProps } from 'react';
 import { cn } from '@/lib/utils';
 
 const Collapsible = CollapsiblePrimitive.Root;
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
 
-const CollapsibleContent = forwardRef<
-  React.ElementRef<typeof CollapsiblePrimitive.CollapsibleContent>,
-  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.CollapsibleContent>
->(({ children, ...props }, ref) => {
+const CollapsibleContent: FC<ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>> = ({
+  children,
+  ref,
+  ...props
+}) => {
   return (
     <CollapsiblePrimitive.CollapsibleContent
       forceMount
-      ref={ref}
       {...props}
       className={cn(
         'overflow-hidden [--radix-collapsible-content-height:0px] data-[state=closed]:hidden data-[state=closed]:animate-[collapse-out_200ms_cubic-bezier(0.3,0.0,0.8,0.15)] data-[state=open]:animate-[collapse-in_250ms_cubic-bezier(0.05,0.7,0.1,1.0)]',
@@ -23,8 +23,6 @@ const CollapsibleContent = forwardRef<
       {children}
     </CollapsiblePrimitive.CollapsibleContent>
   );
-});
-
-CollapsibleContent.displayName = CollapsiblePrimitive.CollapsibleContent.displayName;
+};
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent };
