6dbb4041c921acc8b6134aec664353f15560072f
miles-kt-inkeep <miles.kamingthanassi@inkeep.com>
2026-01-18 19:50:30 -0500

    remove split apis

--- agents-eval-api/src/workflow/index.ts
+++
@@ -1,3 +0,0 @@
-export { evaluateConversationWorkflow } from './functions/evaluateConversation';
-export { runDatasetItemWorkflow } from './functions/runDatasetItem';
-export { world } from './world';
