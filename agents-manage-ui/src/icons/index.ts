65b077c943713eaf3eac589f156aea6fe02274a7
Dima <dima@inkeep.com>
2026-01-21 00:26:29 +0700

    fix agent flow edge arrow

--- agents-manage-ui/src/icons/index.ts
+++ agents-manage-ui/src/icons/index.ts
@@ -1,6 +1,7 @@
 export { default as ArrayIcon } from './array.svg?svgr';
 export { default as BooleanIcon } from './boolean.svg?svgr';
 export { default as EdgeArrow } from './edge-arrow.svg?svgr';
+export { default as SelectedEdgeArrow } from './selected-edge-arrow.svg?svgr';
 export { default as EnumIcon } from './enum.svg?svgr';
 export { default as InkeepLogo } from './inkeep.svg?svgr';
 export { default as NumberIcon } from './number.svg?svgr';
