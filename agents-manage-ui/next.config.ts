efc4da341b4c7bd278df559b78664994cbb3e611
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2026-01-21 16:56:42 +0200

    feat(manage-ui): upload source maps if sentry is enabled

--- agents-manage-ui/next.config.ts
+++ agents-manage-ui/next.config.ts
@@ -13,6 +13,8 @@ if (process.env.NODE_ENV !== 'production') {
   }
 }
 
+const isSentryEnabled = Boolean(process.env.NEXT_PUBLIC_SENTRY_DSN);
+
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_CI: process.env.CI,
@@ -47,30 +49,32 @@ const nextConfig: NextConfig = {
       { protocol: 'http', hostname: '**' },
     ],
   },
+  productionBrowserSourceMaps: isSentryEnabled,
 };
 
-export default process.env.NEXT_PUBLIC_SENTRY_DSN
-  ? withSentryConfig(nextConfig, {
+const config = isSentryEnabled
+  ? withSentryConfig(
+      nextConfig,
       // For all available options, see:
       // https://npmjs.com/package/@sentry/webpack-plugin#options
+      {
+        org: process.env.SENTRY_ORG,
+        project: process.env.SENTRY_PROJECT,
+        // Only print logs for uploading source maps in CI
+        silent: !process.env.CI,
 
-      org: process.env.SENTRY_ORG,
+        // For all available options, see:
+        // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-      project: process.env.SENTRY_PROJECT,
-
-      // Only print logs for uploading source maps in CI
-      silent: !process.env.CI,
-
-      // For all available options, see:
-      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-      // Upload a larger set of source maps for prettier stack traces (increases build time)
-      widenClientFileUpload: true,
-
-      // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-      // This can increase your server load as well as your hosting bill.
-      // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-      // side errors will fail.
-      tunnelRoute: '/monitoring',
-    })
+        // Upload a larger set of source maps for prettier stack traces (increases build time)
+        widenClientFileUpload: true,
+        // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+        // This can increase your server load as well as your hosting bill.
+        // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+        // side errors will fail.
+        tunnelRoute: '/monitoring',
+      }
+    )
   : nextConfig;
+
+export default config;
