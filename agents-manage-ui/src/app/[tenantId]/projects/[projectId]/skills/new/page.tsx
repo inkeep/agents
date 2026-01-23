d942452b538eb0f1747ac51138c4bddac8ea8335
Dima <dima@inkeep.com>
2026-01-24 05:25:20 +0700

    add mutation cache

--- agents-manage-ui/src/app/[tenantId]/projects/[projectId]/skills/new/page.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from 'react';
-import { SkillForm } from '@/components/skills/form/skill-form';
-
-const NewSkillPage: FC = async () => {
-  return <SkillForm />;
-};
-
-export default NewSkillPage;
