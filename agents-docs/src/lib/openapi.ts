34e469f762d807663312da83f40f2dad1d709d62
Dima <dima@inkeep.com>
2026-01-27 02:43:01 +0700

    cleanup old code

--- agents-docs/src/lib/openapi.ts
+++ agents-docs/src/lib/openapi.ts
@@ -3,8 +3,6 @@ import { createOpenAPI } from 'fumadocs-openapi/server';
 export const openapi = createOpenAPI({
   // the OpenAPI schema, you can also give it an external URL.
   input: () => ({
-    index:
-      'https://raw.githubusercontent.com/inkeep/agents/b1662340abdb842477389c13f22382a54c1ede75/agents-docs/src/lib/index.json',
-    'run-api': 'https://agents-run-api.preview.inkeep.com/openapi.json',
+    index: 'https://api.pilot.inkeep.com/openapi.json',
   }),
 });
