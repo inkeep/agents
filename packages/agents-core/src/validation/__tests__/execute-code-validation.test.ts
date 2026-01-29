81bb5cfacf29e9ce52f19923915f8e3296278258
Dima <dima@inkeep.com>
2026-01-29 13:57:31 +0700

    rename

--- packages/agents-core/src/validation/__tests__/execute-code-validation.test.ts
+++ packages/agents-core/src/validation/__tests__/execute-code-validation.test.ts
@@ -1,6 +1,6 @@
 import { FunctionApiInsertSchema } from '../schemas';
 
-function test(executeCode: string) {
+function parse(executeCode: string) {
   return FunctionApiInsertSchema.parse({
     id: 'fn-1',
     executeCode,
@@ -9,60 +9,60 @@ function test(executeCode: string) {
 
 describe('FunctionApiInsertSchema executeCode validation', () => {
   describe('rejects', () => {
-    it('export default function', () => {
-      expect(() => test('export default function() {}')).toThrowError(
+    test('export default function', () => {
+      expect(() => parse('export default function() {}')).toThrowError(
         /Export default is not allowed/
       );
     });
 
-    it('global return', () => {
-      expect(() => test('return 1')).toThrowError(/Global return is not allowed/);
+    test('global return', () => {
+      expect(() => parse('return 1')).toThrowError(/Global return is not allowed/);
     });
 
-    it('TypeScript syntax', () => {
-      expect(() => test('(value: number) => value')).toThrowError(
+    test('TypeScript syntax', () => {
+      expect(() => parse('(value: number) => value')).toThrowError(
         /TypeScript syntax is not allowed/
       );
     });
 
-    it('JSX syntax', () => {
-      expect(() => test('() => <div />')).toThrowError(/JSX syntax is not allowed/);
+    test('JSX syntax', () => {
+      expect(() => parse('() => <div />')).toThrowError(/JSX syntax is not allowed/);
     });
 
-    it('code without functions', () => {
-      expect(() => test('"test"')).toThrowError(/Must have one function/);
+    test('code without functions', () => {
+      expect(() => parse('"test"')).toThrowError(/Must have one function/);
     });
 
-    it('code with 2 and more functions', () => {
-      expect(() => test('function foo() {} function bar() {}')).toThrowError(
+    test('code with 2 and more functions', () => {
+      expect(() => parse('function foo() {} function bar() {}')).toThrowError(
         /Must have one function, but got 2/
       );
     });
   });
 
   describe('allows', () => {
-    it('anonymous function', () => {
-      expect(() => test('function() {}')).not.toThrowError();
+    test('anonymous function', () => {
+      expect(() => parse('function() {}')).not.toThrowError();
     });
 
-    it('async anonymous function', () => {
-      expect(() => test('async function() {}')).not.toThrowError();
+    test('async anonymous function', () => {
+      expect(() => parse('async function() {}')).not.toThrowError();
     });
 
-    it('arrow function', () => {
-      expect(() => test('() => 1')).not.toThrowError();
+    test('arrow function', () => {
+      expect(() => parse('() => 1')).not.toThrowError();
     });
 
-    it('async arrow function', () => {
-      expect(() => test('async () => 1')).not.toThrowError();
+    test('async arrow function', () => {
+      expect(() => parse('async () => 1')).not.toThrowError();
     });
 
-    it('named function', () => {
-      expect(() => test('function foo() {}')).not.toThrowError();
+    test('named function', () => {
+      expect(() => parse('function foo() {}')).not.toThrowError();
     });
 
-    it('async named function', () => {
-      expect(() => test('async function foo() {}')).not.toThrowError();
+    test('async named function', () => {
+      expect(() => parse('async function foo() {}')).not.toThrowError();
     });
   });
 });
