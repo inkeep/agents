2faa376ed563dffbb75c6510cc2ef97bffeda02f
Dima <dima@inkeep.com>
2026-02-03 01:50:59 +0700

    set resolveDiffPath

--- agents-manage-ui/vitest.config.ts
+++ agents-manage-ui/vitest.config.ts
@@ -1,9 +1,27 @@
 import { fileURLToPath } from 'node:url';
 import { playwright } from '@vitest/browser-playwright';
 import { defaultExclude, defineConfig } from 'vitest/config';
+import type { ToMatchScreenshotOptions } from 'vitest/node';
 import pkgJson from './package.json' with { type: 'json' };
 
 const NODE_TESTS_PATTERN = '**/*.node.test.ts';
+
+const resolveScreenshotPath: ToMatchScreenshotOptions['resolveScreenshotPath'] = ({
+  root,
+  screenshotDirectory,
+  arg,
+  browserName,
+  ext,
+}) => {
+  return [
+    root,
+    'src',
+    screenshotDirectory,
+    // Omit the platform suffix before extension
+    // `linux` on CI and `darwin` for example in macOS
+    `${arg}-${browserName}${ext}`,
+  ].join('/');
+};
 
 export default defineConfig({
   test: {
@@ -13,34 +31,19 @@ export default defineConfig({
       {
         extends: true,
         test: {
-          name: `${pkgJson.name}/node`,
-          setupFiles: './setup-files',
-          include: [NODE_TESTS_PATTERN],
-        },
-      },
-      {
-        extends: true,
-        test: {
           name: `${pkgJson.name}/browser`,
           exclude: [NODE_TESTS_PATTERN, ...defaultExclude],
           browser: {
-            instances: [{ browser: 'chromium' }],
-            provider: playwright(),
             enabled: true,
             headless: true,
+            provider: playwright(),
+            instances: [{ browser: 'chromium' }],
             expect: {
               toMatchScreenshot: {
                 // Increase timeout because the default `5s` is insufficient on CI
                 timeout: 15_000,
-                resolveScreenshotPath: ({ root, screenshotDirectory, arg, browserName, ext }) =>
-                  [
-                    root,
-                    'src',
-                    screenshotDirectory,
-                    // Omit the platform suffix before extension
-                    // `linux` on CI and `darwin` for example in macOS
-                    `${arg}-${browserName}${ext}`,
-                  ].join('/'),
+                resolveScreenshotPath,
+                resolveDiffPath: resolveScreenshotPath,
               },
             },
           },
@@ -48,6 +51,14 @@ export default defineConfig({
         define: {
           // Fix error from next/image - ReferenceError: process is not defined
           'process.env.NODE_ENV': '"test"',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: `${pkgJson.name}/node`,
+          setupFiles: './setup-files',
+          include: [NODE_TESTS_PATTERN],
         },
       },
     ],
