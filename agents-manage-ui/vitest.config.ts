f71ea30f68b0f980917cf4f4c154fbf3f7c6da6c
Dima <dima@inkeep.com>
2026-02-02 14:36:53 +0700

    upd deps

--- agents-manage-ui/vitest.config.ts
+++ agents-manage-ui/vitest.config.ts
@@ -11,7 +11,7 @@ export default defineProject({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('src', import.meta.url)),
     },
   },
 });
