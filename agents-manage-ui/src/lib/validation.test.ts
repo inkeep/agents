8087665df7bf4b9a62d62f356ecd2f0527e3d768
Dima <dima@inkeep.com>
2026-02-05 19:02:04 +0700

    wip tests

--- agents-manage-ui/src/lib/validation.test.ts
+++ agents-manage-ui/src/lib/validation.test.ts
@@ -1,3 +1,36 @@
+import { createCustomHeadersSchema } from '@/lib/validation';
+
 describe('validation', () => {
-  test('createCustomHeadersSchema', () => {});
+  describe('createCustomHeadersSchema', () => {
+    test('should throw when not object', () => {
+      const schema = createCustomHeadersSchema('');
+      expect(() => schema.parse('null')).toThrowError('Must be valid JSON object');
+    });
+    test('should throw when invalid syntax', () => {
+      const schema = createCustomHeadersSchema('');
+      expect(() => schema.parse('#')).toThrowError('Invalid JSON syntax');
+    });
+    test('should throw nested keys', () => {
+      const schema = createCustomHeadersSchema('');
+      expect(() =>
+        schema.parse(
+          JSON.stringify({
+            foo: {
+              bar: 'baz',
+            },
+          })
+        )
+      ).toThrowError('All header values must be strings\\n  → at foo');
+    });
+    test('should throw when key is not object', () => {
+      const schema = createCustomHeadersSchema('');
+      expect(() =>
+        schema.parse(
+          JSON.stringify({
+            foo: null,
+          })
+        )
+      ).toThrowError('All header values must be strings\\n  → at foo');
+    });
+  });
 });
