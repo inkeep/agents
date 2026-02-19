28d2f71815baa22e9fc3df5dd26df5ed9ce4eeeb
Dima <dima@inkeep.com>
2026-02-19 19:45:16 +0700

    upd

--- agents-cli/src/commands/pull-v4/utils.test.ts
+++ agents-cli/src/commands/pull-v4/utils.test.ts
@@ -1,4 +1,4 @@
-import { toCamelCase } from './utils';
+import { formatStringLiteral, toCamelCase } from './utils';
 
 describe('camelCase', () => {
   test('should handle special characters', () => {
@@ -11,5 +11,21 @@ describe('camelCase', () => {
     expect(toCamelCase('status.config')).toBe('statusConfig');
     expect(toCamelCase('statuS.config')).toBe('statuSConfig');
     expect(toCamelCase('statuS0.config')).toBe('statuS0Config');
+  });
+});
+
+describe('formatStringLiteral', () => {
+  test('should use single quotes by default', () => {
+    expect(formatStringLiteral('find 3 urls')).toBe("'find 3 urls'");
+  });
+
+  test('should use double quotes when value contains a single quote', () => {
+    expect(formatStringLiteral("user's question")).toBe(`"user's question"`);
+  });
+
+  test('should use a template literal when value contains both single and double quotes', () => {
+    expect(formatStringLiteral(`find 3 URLs relevant to the user's "question".`)).toBe(
+      '`find 3 URLs relevant to the user\'s "question".`'
+    );
   });
 });
