a89812515bc855bde83d7b4d0694bf16cbd997e7
Dima <dima@inkeep.com>
2026-02-26 03:33:30 +0700

    generic checkbox

--- agents-manage-ui/src/components/form/generic-checkbox.tsx
+++ agents-manage-ui/src/components/form/generic-checkbox.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import type { ReactNode, JSX } from 'react';
-import type { Control, ControllerRenderProps, FieldPath, FieldValues } from 'react-hook-form';
+import type { Control, FieldPath, FieldValues } from 'react-hook-form';
 import {
   FormControl,
   FormDescription,
@@ -10,6 +10,7 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
+import { Checkbox } from '@/components/ui/checkbox';
 
 export interface FormFieldWrapperProps<
   FV extends FieldValues,
@@ -19,7 +20,6 @@ export interface FormFieldWrapperProps<
   control: Control<FV, unknown, TV>;
   name: TName;
   label: string | JSX.Element;
-  children: (field: ControllerRenderProps<FV, TName>) => ReactNode;
   description?: ReactNode;
   isRequired?: boolean;
 }
@@ -28,22 +28,19 @@ export function GenericCheckbox<
   FV extends FieldValues,
   TV extends FieldValues,
   TName extends FieldPath<FV>,
->({
-  control,
-  name,
-  label,
-  children,
-  description,
-  isRequired,
-}: FormFieldWrapperProps<FV, TV, TName>) {
+>({ control, name, label, description, isRequired }: FormFieldWrapperProps<FV, TV, TName>) {
   return (
     <FormField
       control={control}
       name={name}
       render={({ field }) => (
-        <FormItem className="relative">
-          <FormLabel isRequired={isRequired}>{label}</FormLabel>
-          <FormControl>{children(field)}</FormControl>
+        <FormItem>
+          <div className="flex gap-2">
+            <FormControl>
+              <Checkbox checked={field.value} onCheckedChange={field.onChange} />
+            </FormControl>
+            <FormLabel isRequired={isRequired}>{label}</FormLabel>
+          </div>
           {description && <FormDescription>{description}</FormDescription>}
           <FormMessage />
         </FormItem>
