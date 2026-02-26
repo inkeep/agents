b3b7c998af65bb340516a501cacd052d343b4a0c
Dima <dima@inkeep.com>
2026-02-26 12:55:08 +0700

    add generic code editor

---
+++ agents-manage-ui/src/components/form/generic-code-editor.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { type ComponentProps, useState } from 'react';
+import type { FieldPath, FieldValues } from 'react-hook-form';
+import { Editor } from '@/components/editors/editor';
+import { CodeEditor } from '@/components/editors/code-editor';
+import type { FormFieldWrapperProps } from '@/components/form/form-field-wrapper';
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { cn } from '@/lib/utils';
+
+export function GenericCodeEditor<
+  FV extends FieldValues,
+  TV extends FieldValues,
+  TName extends FieldPath<FV>,
+>({
+  control,
+  name,
+  description,
+  isRequired,
+  label,
+  className,
+  placeholder,
+  ...props
+}: Omit<FormFieldWrapperProps<FV, TV, TName>, 'children'> & {
+  className?: string;
+  placeholder: string;
+  uri?: ComponentProps<typeof CodeEditor>['uri'];
+}) {
+  'use memo';
+  const [open, onOpenChange] = useState(false);
+  const $uri = props.uri ?? `${name}.js`;
+  const uri = `${open ? 'expanded-' : ''}${$uri}` as const;
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <Editor.Dialog open={open} onOpenChange={onOpenChange} label={label}>
+            <div className="flex">
+              <FormLabel isRequired={isRequired} className="inline-flex grow">
+                {label}
+              </FormLabel>
+              {!open && <Editor.DialogTrigger />}
+            </div>
+            <FormControl>
+              <CodeEditor
+                uri={uri}
+                autoFocus={open}
+                className={cn(!open && 'max-h-96', 'min-h-16', className)}
+                hasDynamicHeight={!open}
+                placeholder={placeholder}
+                // aria-labelledby={id}
+                {...field}
+              />
+            </FormControl>
+            {description && <FormDescription>{description}</FormDescription>}
+            <FormMessage />
+          </Editor.Dialog>
+        </FormItem>
+      )}
+    />
+  );
+}
