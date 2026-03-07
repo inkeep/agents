d4fbee2225c00d06b60e007b771ed76c4fd976b3
Dima <dima@inkeep.com>
2026-03-07 22:27:16 +0700

    upd errors styles

--- agents-manage-ui/src/components/agent/toolbar.tsx
+++ agents-manage-ui/src/components/agent/toolbar.tsx
@@ -125,7 +125,7 @@ export function Toolbar({ toggleSidePane, setShowPlayground }: ToolbarProps) {
           onClick={toggleSidePane}
           className={cn(commonProps.className, hasErrors && 'ring-2 text-red-300! border-current!')}
         >
-          <Settings className={hasErrors ? '' : 'text-muted-foreground'} />
+          <Settings className={cn(!hasErrors && 'text-muted-foreground')} />
           Agent Settings
           {hasErrors && <ErrorIndicator errors={agentSettingsErrors} />}
         </Button>
