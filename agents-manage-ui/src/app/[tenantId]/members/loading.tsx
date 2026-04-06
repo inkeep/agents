668f82fc84147ca3ecfeb26e307e422e7b07ba1d
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2026-04-07 06:44:26 +0700

    Delete agents-manage-ui/src/app/[tenantId]/members/loading.tsx

--- agents-manage-ui/src/app/[tenantId]/members/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Skeleton } from '@/components/ui/skeleton';
-
-export default function MembersLoadingSkeleton() {
-  return (
-    <div className="space-y-6">
-      <Skeleton className="h-64 w-full rounded-lg" />
-    </div>
-  );
-}
